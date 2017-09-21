a2dd08c9ad81ab3647a8a2eef0770b7f3ea62559
ozaman07 <semi_clever@hotmail.com>
2017-09-22 01:08:08 +0700

    c

--- main/add-booking.php
+++ main/add-booking.php
@@ -147,6 +147,12 @@
                                          Account
                                     </a>
                                 </li>
+                                 <li>
+                                    <a href="../report/manage-tour.php">
+                                    <i class="material-icons">account_balance_wallet</i>
+                                         Manage Account Tours
+                                    </a>
+                                </li>
                             </ul>
                         </div>
                     </li>
