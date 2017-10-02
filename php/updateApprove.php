00964774b86d64ae88262b3eba8379f5667d9161
ozaman07 <semi_clever@hotmail.com>
2017-10-03 00:57:36 +0700

    c

--- php/updateApprove.php
+++ php/updateApprove.php
@@ -1,7 +1,7 @@
 <?php
 include("../config.php");
                             //echo $finalcode;
-      $sql = "UPDATE dn_booking SET approved = '1',done = '1', approve_by = '".$_POST[approve_by]."' WHERE id = '".$_POST[id]."' " ;
+      $sql = "UPDATE dn_booking SET approved = '1',done = '1', approve_by = '".$_POST[approve_by]."',approve_date = '".time()."' WHERE id = '".$_POST[id]."' " ;
       $Objquery = mysql_query($sql,$conndb);
       if($Objquery){
                 
